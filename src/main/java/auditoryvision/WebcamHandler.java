eca53dc86adb93e21d4b4306d599a450bfee22fb
Christopher Wells <cwellsny@nycap.rr.com>
2017-02-11 14:54:32 -0500

    Set webcam image to max size

--- src/main/java/auditoryvision/WebcamHandler.java
+++ src/main/java/auditoryvision/WebcamHandler.java
@@ -3,6 +3,7 @@ package auditoryvision;
 import com.github.sarxos.webcam.Webcam;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,6 +13,9 @@ import java.io.IOException;
 public class WebcamHandler {
     public static File capture(String capturePath) throws IOException {
         Webcam webcam = Webcam.getDefault();
+        Dimension[] dimensions = webcam.getViewSizes();
+        Dimension largest = dimensions[dimensions.length - 1];
+        webcam.setViewSize(largest);
         webcam.open();
         File file = new File(capturePath);
         ImageIO.write(webcam.getImage(), "jpg", file);
