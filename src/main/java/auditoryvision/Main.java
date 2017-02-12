d9e3fc0c77fc4a7ce9ba55bf7b4e9cd957bd5872
Christopher Wells <cwellsny@nycap.rr.com>
2017-02-11 21:15:47 -0500

    Change program to work via an api

--- src/main/java/auditoryvision/Main.java
+++ src/main/java/auditoryvision/Main.java
@@ -2,26 +2,38 @@ package auditoryvision;
 
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 
+import static spark.Spark.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String key = sc.next();
         IBMapi api = new IBMapi(key);
 
-        String capturePath = "image.jpg";
+        get("/search", (req, res) -> search(api));
+    }
 
-        System.out.println("Taking picture");
-        File file = WebcamHandler.capture(capturePath);
+    public static String search(IBMapi api) {
+        try {
+            String capturePath = "image.jpg";
 
-        System.out.println("Querying api");
-        VisualClassification imageInfo = api.getImageInfo(file);
+            System.out.println("Taking picture");
+            File file = WebcamHandler.capture(capturePath);
 
-        String description = Description.createDescription(imageInfo);
-        System.out.println(description);
+            System.out.println("Querying api");
+            VisualClassification imageInfo = api.getImageInfo(file);
+
+            String description = Description.createDescription(imageInfo);
+            System.out.println(description);
+
+            return description;
+        } catch (Exception e) {
+            return "Whoops!";
+        }
     }
 }
